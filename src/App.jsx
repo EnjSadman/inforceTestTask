afe8621431af1fe6a79f4461380ebff575e34bc0
Roman Pavlenko <roman.pavlenko@qbex.io>
2023-10-05 14:00:51 +0300

    solution

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.scss';
+import { ProductList } from './components/ProductList';
+
+function App() {
+  return (
+    <div className="App">
+      <ProductList />
+    </div>
+  );
+}
+
+export default App;
